32e37de5b423558ab53c21417a70d2484f26ceed
Sergii Sergiichuk <spsembler@gmail.com>
2013-08-02 15:30:58 +0300

    Moskito Features - Added Carousel animation

--- static-int/js/common.js
+++ static-int/js/common.js
@@ -1,3 +1,84 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $('.carousel').carousel();
+    $('#myTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    })
+    $(window).scroll(function(){
+        if ( $(this).scrollTop() > 30){
+            $('body').addClass("scroll");
+        } else if($(this).scrollTop() <= 30 && $('body').hasClass("scroll")) {
+            $('body').removeClass("scroll");
+        }
+    });//scroll
 
+    $("#carousel").carouFredSel({
+        scroll: {
+            fx: "crossfade",
+            duration: 700
+        }
+    });
+
+    $(".features-list ul").carouFredSel({
+        width   : "100%",
+        items	: 4,
+        scroll	: {
+            items: 2,
+            duration: 700
+        }
+    });
+
+    $(window).resize(function(){
+        $(".features-list ul").carouFredSel({
+            width   : "100%",
+            items	: 4,
+            scroll	: {
+                items: 2,
+                duration: 700
+            }
+        });
+        if ($(window).width() < 979) {
+            $(".features-list ul").carouFredSel({
+                width   : "100%",
+                items	: 3,
+                scroll	: {
+                    items: 2,
+                    duration: 700
+                }
+            });
+        }
+        if ($(window).width() < 768) {
+            $(".features-list ul").carouFredSel({
+                width   : "100%",
+                items	: 2,
+                scroll	: {
+                    items: 2,
+                    duration: 700
+                }
+            });
+        }
+    });
+
+    if ($(window).width() < 979) {
+        $(".features-list ul").carouFredSel({
+            width   : "100%",
+            items	: 3,
+            scroll	: {
+                items: 2,
+                duration: 700
+            }
+        });
+    }
+    if ($(window).width() < 768) {
+        $(".features-list ul").carouFredSel({
+            width   : "100%",
+            items	: 2,
+            scroll	: {
+                items: 2,
+                duration: 700
+            }
+        });
+    }
+
+    $().UItoTop({ easingType: 'easeOutQuart', text: '<i class="icon-angle-up"></i>' });
 });
