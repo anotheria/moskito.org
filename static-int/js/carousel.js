be412cb50a252d824f903e7f0b41b08fba75c8c4
Sergii Sergiichuk <spsembler@gmail.com>
2013-08-09 11:01:11 +0300

    added tooltip to control & cnetral

---
+++ static-int/js/carousel.js
@@ -0,0 +1,140 @@
+$(document).ready(function() {
+
+    $("#carousel").carouFredSel({
+        width: '100%',
+        height: '100%',
+        scroll: {
+            fx: "crossfade",
+            duration: 1000,
+            timeoutDuration: 7000
+
+        },
+        auto    : {
+            pauseOnHover: 'resume'
+        },
+        prev: {
+            button: "#carouselPrev",
+            key: "left"
+        },
+        next: {
+            button: "#carouselNext",
+            key	: "right"
+        }
+    });
+
+    $(".features-list ul").carouFredSel({
+        width   : "100%",
+        items	: 3,
+        scroll	: {
+            items: 2,
+            duration: 1000,
+            timeoutDuration: 7000
+        },
+        auto    : {
+            pauseOnHover: 'resume'
+        },
+        swipe: {
+            onTouch: true,
+            onMouse: true
+        },
+        pagination  : ".features-pagination"
+    });
+
+    $(window).resize(function(){
+        $(".features-list ul").carouFredSel({
+            width   : "100%",
+            items	: 3,
+            scroll	: {
+                items: 2,
+                duration: 1000,
+                timeoutDuration: 7000
+            },
+            auto    : {
+                pauseOnHover: 'resume'
+            },
+            swipe: {
+                onTouch: true,
+                onMouse: true
+            },
+            pagination  : ".features-pagination"
+        });
+        if ($(window).width() < 979) {
+            $(".features-list ul").carouFredSel({
+                width   : "100%",
+                items	: 2,
+                scroll	: {
+                    items: 2,
+                    duration: 1000,
+                    timeoutDuration: 7000
+                },
+                auto    : {
+                    pauseOnHover: 'resume'
+                },
+                swipe: {
+                    onTouch: true,
+                    onMouse: true
+                },
+                pagination  : ".features-pagination"
+            });
+        }
+        if ($(window).width() < 768) {
+            $(".features-list ul").carouFredSel({
+                width   : "100%",
+                items	: 2,
+                scroll	: {
+                    items: 2,
+                    duration: 1000,
+                    timeoutDuration: 7000
+                },
+                auto    : {
+                    pauseOnHover: 'resume'
+                },
+                swipe: {
+                    onTouch: true,
+                    onMouse: true
+                },
+                pagination  : ".features-pagination"
+            });
+        }
+    });
+
+    if ($(window).width() < 979) {
+        $(".features-list ul").carouFredSel({
+            width   : "100%",
+            items	: 2,
+            scroll	: {
+                items: 2,
+                duration: 1000,
+                timeoutDuration: 7000
+            },
+            auto    : {
+                pauseOnHover: 'resume'
+            },
+            swipe: {
+                onTouch: true,
+                onMouse: true
+            },
+            pagination  : ".features-pagination"
+        });
+    }
+    if ($(window).width() < 768) {
+        $(".features-list ul").carouFredSel({
+            width   : "100%",
+            items	: 2,
+            scroll	: {
+                items: 2,
+                duration: 1000,
+                timeoutDuration: 7000
+            },
+            auto    : {
+                pauseOnHover: 'resume'
+            },
+            swipe: {
+                onTouch: true,
+                onMouse: true
+            },
+            pagination  : ".features-pagination"
+        });
+    }
+
+});
